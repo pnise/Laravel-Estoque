5a8ff9183c426d9b0c2bd0d04c4b23f1e2604506
Rafael Nise <rafael.pereira@ibiz.com.br>
2018-01-17 01:08:06 -0200

    Laravel Projeto Estoque

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -1,8 +1,11 @@
-<?php namespace estoque\Http\Controllers;
+<?php 
 
-class  ProdutoController{
+namespace estoque\Http\Controllers;
+
+class  ProdutoController extends Controller{
 
 	public function lista(){
 		//nosso codigo vai aqui
+		return '<h1>Listagem de produtos com Laravel</h1>';
 	}
 }
